992b435b8c58b1d8d3497d9c26ec86c1262fc6eb
Suga T <sugananth2003@gmail.com>
2017-08-26 01:23:51 -0500

    late error

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,11 +3,13 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { ProductComponent } from './product/product.component';
+import { NewCmpComponent } from './new-cmp/new-cmp.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    ProductComponent
+    ProductComponent,
+    NewCmpComponent
   ],
   imports: [
     BrowserModule
